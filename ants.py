e818fd2ef7a94465dcac80a972e7c8489a7adcb5
Alex Mineeva <amineeva@olin.edu>
2026-01-28 17:10:38 -0500

    Added exploratory ant functions for turning: can now get a new turned or forward heading and know which 'block' (0-7) to move to based on turn delta

--- ants.py
+++ ants.py
@@ -1,24 +1,34 @@
 # File containing ant class, move one grid space per turn.
 
+# imports
+import numpy as np
+
 
 class Ant:
     """
     Represents an ant agent on the grid. 
 
     Attributes:
-        x: Int representing x-location at current timestep.
-        y: Int representing y-location at current timestep.
-        direction: Int representing direction angle of last movement; movement to current timestep.
+        x: Int representing x-location at current timestep. Default is 128.
+        y: Int representing y-location at current timestep. Default is 128.
+        direction: Int representing direction of the ant's movement from the previous timestep to current timestep; orients what direction is "forward".
         state: String representing state of ant: 'explorer' or 'follower'.
+        B: Tuple representing the turning kernels (B1, B2, B3, B4). Default (0.25, 0.25, 0.25, 0.25).
+        p_straight: Float between 0 and 1 representing the probability that an exploratory ant will go forward rather than turn. Default is 0.5 (50%).
     """
 
-    def __init__(self, x, y):
+    def __init__(self, x = 128, y = 128, B = (0.25, 0.25, 0.25, 0.25), p_straight = 0.5):
         self.x = x
         self.y = y
+        self.B = B
+        self.p_straight = p_straight
+        self.direction = np.random.randint(0, 8)
+        self.state = 'explorer' # self.determine_state() - WIP!! function is being implemented later, this is a placeholder FOR NOW
 
     def __repr__(self):
         return f"x-loc: {self.x}, y-loc: {self.y}"
     
+    # 'Set' functions
     def set_location(self, x_new, y_new):
         """Updates x and y location of ant to new x and y location."""
         self.x = x_new
@@ -29,36 +39,78 @@ class Ant:
         self.state = state_new
 
     def set_direction(self, direction_new):
-        """Updates ant direction to new angle."""
+        """set ant direction to new angle."""
         self.direction = direction_new
 
 
-# Functions #
+    ###### Exploratory Movement ######
+    def new_random_delta_turn(self):
+        """
+        Function determines if ant will move straight or randomly turn.
 
- # 50/50 chance for turning left or right
-def determine_new_direction_angle(Ant):
- """
- Function determines new angle to rotate. 0 degrees is straight, 180 degrees
+        Args:
+            self: Ant object representing the ant.
 
- Args:
+        Returns:
+            delta_turn: Int of value +/- 1, 2, 3, 4, 0 representing the number of 45 degree units to turn. If straight, the value is 0.
+        """
 
- Returns:
- """
+        if np.random.rand() < self.p_straight:
+            delta_turn = 0
+        else:
+            delta_turn = angle_of_turn(self.B)
+        return delta_turn
+    
+    def update_direction(self):
+        """
+        Function updates ant direction with new delta_turn. Determines the new "forward" direction in terms of 45 degree turn units (0-7). Positive is clockwise.
+        0: Forward
+        1: Upper right (45 degrees)
+        2: Right (90 degrees)
+        3. Bottom right (135 degrees)
+        4. Bottom/backward (180 degrees)
+        5. Bottom left (225 degrees)
+        6. Left (270 degrees)
+        7. Upper left (315 degrees)
+        
+        Args:
+            self: Ant object representing the ant.
+        
+        Returns:
+            None
+        """
+        delta_turn = self.new_random_delta_turn()
 
- def establish_simulation(system, orbiting_objects_dictionary, time):
+        new_direction = (self.direction + delta_turn) % 8 # the remainder here turns the left "negative" delta_turns into 4, 5, 6, 7. 
+        
+        self.set_direction(new_direction)
+
+
+
+    # State determination function placeholder - will implement when multiple ants/second round of simulation
+    def determine_state(self):
+        """WIP - Will return string representing ant's 'follower' or 'explorer' state."""
+
+
+
+########### HELPER FUNCTIONS #########
+
+def angle_of_turn(B):
     """
-    Function used within run_simulation in order to create the initial system vectors. Defines intial position conditions. 
+    Function generates a new random turn angle for an explorer ant's change in direction.
 
     Args:
-        orbiting_objects_dictionary: Dictionary of objects, orbiting within system.
-        time: Numpy array (vector) holding a timestep-ed time vector in years
-
+        B: Tuple representing the turning kernels (B1, B2, B3, B4). Default (0.25, 0.25, 0.25, 0.25).
     Returns:
-        positions: Dictionary for x, y, z positions of each orbiting object
-        velocities: Dictionary for x, y, z velocities of each orbiting object
-        angular_velocities: Dictionary for angular velocity of each orbiting object
-        orbit_radii: Dictionary for distances between system center and each orbiting object
-        parent: Dictionary with parent of object - only applies to Orbital Systems within Orbital Systems
+        angle_amount * turn_direction: Int of value +/- 1, 2, 3, 4 representing the number of 45 degree units to turn, and in which direction.
     """
-    num_steps = len(time)
+
+    # use turning kernel B to randomize how many 45 degree directions the ant turns
+    turn_angles = [1, 2, 3, 4] # each value represents turns in 45 degree increments; 0 is forward.
+    angle_amount = np.random.choice(turn_angles, p = B)
+
+    # randomize left or right, 50% chance each way
+    turn_direction = np.random.choice([-1, 1])
+
+    return (angle_amount * turn_direction)
     
