3b173cb5ea28a8b66df937ddaa568e63948f73d6
Alex Mineeva <amineeva@olin.edu>
2026-02-08 12:19:33 -0500

    adjusting debgugging visualization input

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ tau = 12 # according to Fig 3a from the paper
 grid_size = 256 # according to paper
 num_steps = 100 # according to Fig 3a description from paper
 verbose = False
-live_vis = True
+live_vis = False
 
 # Run simulation
 sr.run_simulation(grid_size, fidelity, tau, num_steps, verbose, live_vis)
